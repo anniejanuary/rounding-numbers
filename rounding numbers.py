c2ebac8a758388c5b6cad2f6c7a4bddb90f44934
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-21 14:06:51 +0200

    Update rounding numbers.py

--- rounding numbers.py
+++ rounding numbers.py
@@ -1,1 +1,14 @@
+print(8/3)
+# = 2.6666666666666665
+
 print(round(8/3))
+# = 3
+
+print(8//3)
+# = 2 (INT)
+
+print(round(8/3,2))
+# = 2.67, rounding to ",x" decimal placecs
+
+print(round(2.6666666666666665, 2))
+# = 2.67
